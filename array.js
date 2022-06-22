d75a3399021aebb807ff74fff63c6d9333aa007b
Areeb-IT <areeb.gondal1@gmail.com>
2022-06-22 00:46:59 -0700

    javascript

---
+++ array.js
@@ -0,0 +1,48 @@
+//how to create an array
+//const fruits=["banana","pineapple","grapes"]
+//console.log((fruits));
+//to access a single element in an array
+//const fruits=["banana","pineapple","grapes"]
+//let req=fruits[2]
+//console.log(req);
+//to change element by index
+//const fruits=["banana","pineapple","grapes"]
+//fruits[1]="apple"
+//console.log(fruits)
+//Objects use names to access its "members". In this example, person.firstName returns John
+//const person = {firstName:"John", lastName:"Doe", age:46};
+//console.log(person.firstName);
+//to find the length of an array we use name.length
+//const person = ["John", "Doe", 46]
+//let length= person.length
+//console.log(person.length);
+//to access the last element we use -1
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let fruit = fruits[fruits.length - 2];
+//console.log(fruit);
+
+//const arr=[0,2,5,54,6]
+//for(var i=0;i<arr.length;i++){
+//    console.log(arr[i]);
+//}
+//add element in an array
+//const fruits = ["Banana", "Orange", "Apple"];
+//fruits.push("orange")
+//console.log(fruits);
+//New element can also be added to an array using the length property
+//const fruits=["banana","peach","orange"]
+//fruits [fruits.length]="apple"
+//console.log(fruits);
+//adding elemetns with high index can create empty spaces
+//const fruits = ["Banana", "Orange", "Apple"];
+//fruits[6] = "Lemon";
+//console.log(fruits);
+//assosiatve array
+//const person = [];
+//person[0] = "John";
+//person[1] = "Doe";
+//person[2] = 46;
+//person.length;   person[0]; 
+//console.log(person[0]);
+//yo recognize we use Array.isArray(name)or instenceof Array but not typeof it can only be used for objects
+ 
